60f6d8cfd47a09b78de1245e4d130e0882de88b8
Artem <art.sled.ua@gmail.com>
2023-10-25 23:51:17 +0300

    fix

--- main.js
+++ main.js
@@ -14,22 +14,22 @@ Powered by Technology</h1>
 <div class="card__item border-one">
   <h2 class="head__title">Supervisor</h2>
   <p class="card__discription">Monitors activity to identify project roadblocks</p>
-  <img src="./img/icon-supervisor.svg" alt="">
+  <img src="img/icon-supervisor.svg" alt="">
 </div>
 <div class="card__item border-two">
   <h2 class="head__title">Team Builder</h2>
   <p class="card__discription">Scans our talent network to create the optimal team for your project</p>
-  <img src="./img/icon-team-builder.svg" alt="">
+  <img src="img/icon-team-builder.svg" alt="">
 </div>
 <div class="card__item border-tree">
   <h2 class="head__title">Karma</h2>
   <p class="card__discription">Regularly evaluates our talent to ensure quality</p>
-  <img src="./img/icon-karma.svg" alt="">
+  <img src="img/icon-karma.svg" alt="">
 </div>
 <div class="card__item border-x">
   <h2 class="head__title">Calculator</h2>
   <p class="card__discription">Uses data from past projects to provide better delivery estimates</p>
-  <img src="./img/icon-calculator.svg" alt="">
+  <img src="img/icon-calculator.svg" alt="">
 </div>
 </div>
 `
